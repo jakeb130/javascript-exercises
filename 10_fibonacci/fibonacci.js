bc545aadfd8ad60a7b6f8564f53d5a65086a10bd
jakeb130 <jakebutts130@gmail.com>
2023-05-04 21:42:37 -0500

    Add own solution for exercise 10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n <= 0) return "OOPS";
+  if (n == 1) return 1;
 
+  let num1 = 0;
+  let num2 = 1;
+  let num3;
+
+  for (i = 1; i < n; i++) {
+    num3 = num1 + num2;
+    num1 = num2;
+    num2 = num3;
+  }
+  return num3;
 };
 
 // Do not edit below this line
